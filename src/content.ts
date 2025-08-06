b6dda6b5f28b968cc715e07e334d2c586ee41732
go-dima <dima.gonikman@gmail.com>
2025-08-06 08:19:41 +0300

    Drop title change on jenkins

--- src/content.ts
+++ src/content.ts
@@ -2,48 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import JenkinsButtons from "./popup/JenkinsButtons";
 import "./styles/content.css";
-import { fetchJson } from "./utils";
-
-const changeTitle = async () => {
-  const url = new URL(window.location.href);
-  const pathParts = url.pathname
-    .split("/")
-    .filter((part) => part && part != "job");
-  if (pathParts.length >= 3) {
-    const jobName = pathParts[1];
-    const branch = pathParts[2];
-
-    const buildPart = pathParts[3] ? ` #${pathParts[3]}` : "";
-    document.title = `${jobName} [${branch}${buildPart}]`;
-  }
-
-  fetchJson(url.href)
-    .then((asJson) => {
-      const lastBuildUrl = asJson?.lastBuild?.url;
-      if (!lastBuildUrl) {
-        return;
-      }
-
-      fetchJson(lastBuildUrl).then((lastBuildAsJson) => {
-        const { inProgress, result } = lastBuildAsJson;
-        const statusToEmoji: Record<string, string> = {
-          SUCCESS: "✅",
-          FAILURE: "❌",
-          ABORTED: "⛔",
-        };
-
-        const emoji = (inProgress ? "🔄" : statusToEmoji[result]) || "🤷‍♂️";
-        document.title = emoji + " " + document.title;
-      });
-    })
-    .catch(() => {});
-};
-
-// check that the href matches jenkins.*.dev
-const jenkinsRegex = /jenkins\..*\.dev/;
-if (jenkinsRegex.test(window.location.href)) {
-  changeTitle();
-}
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (!message.inPrPage) {
